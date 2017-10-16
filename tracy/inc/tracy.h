1b856adeceb1903d004a0b85bd2dbd2ffa4e29a0
Bengtsson <ria34843@ws347.diamond.ac.uk>
2017-10-16 11:16:25 +0100

    Code clean-up.

--- tracy/inc/tracy.h
+++ tracy/inc/tracy.h
@@ -9,10 +9,6 @@
 */
 
 
-#define DOF  (ss_dim/2)
- 
-#define M_PI 3.14159265358979323846  // pi
-
 #ifndef LONG_MAX
 # define LONG_MAX ((long)(((unsigned long) -1) >> 1))
 # define LONG_MIN (~LONG_MAX)
@@ -22,74 +18,17 @@
 
 #define S_SIZE 200  // max size for file name of a lattice file
 
-#define nv_          6
-
 #define blankname    "               "
 
 #define maxincl      5
 #define maxfil       10
 #define bigvectmax   4096
 
-#define maxtransfmat 2000
-#define maxkicks     5
-
-
-const double c0    = 2.99792458e8;             // speed of light in vacuum
-const double q_e   = 1.602e-19;                // electron charge
-const double m_e   = 0.51099906e6;             // electron rest mass [eV/c^2]
-const double mu_0  = 4.0*M_PI*1e-7;            // permittivity of free space
-const double eps_0 = 1.0/(sqr(c0)*mu_0);       // permeability of free space
-const double r_e   = q_e/(4.0*M_PI*eps_0*m_e); // classical electron radius
-const double h_bar = 6.58211899e-16;           // reduced Planck constant [eV s]
-
-const double max_ampl = 10.0; // [m]
-
-
-enum pthicktype { thick = 0, thin = 1 };
-
-enum PartsKind { drift = 0, Wigl = 1, Mpole = 2, Cavity = 3, marker = 4,
-		 undef = 5, Insertion = 6, FieldMap = 7,
-                 Spreader = 8, Recombiner = 9, Solenoid = 10 };
-
-enum { All = 0, Dip = 1, Quad = 2, Sext = 3, Oct = 4, Dec = 5, Dodec = 6 };
-
-enum { Horizontal = 1, Vertical = 2 };
-
-enum { Meth_Linear = 0, Meth_First = 1, Meth_Second = 2, Meth_Fourth = 4,
-       Meth_genfun = 5 };
-
-#define fitvectmax   200
-#define NameLength   150  // maximum length of identifiers (e.g. file names)
-#define DBNameLen    39
-#define HOMmax       21
-#define SymbolLength 15   // maximum length of element name
-
-typedef char str80[80];
-typedef char alfa_[NameLength];
-
-typedef long   iVector2[2];
-typedef double Vector2[2];
-typedef double Vector3[3];
-
-typedef long   fitvect[fitvectmax];
-typedef char   partsName[NameLength];
-typedef char   DBNameType[DBNameLen];
-typedef double mpolArray[HOMmax+HOMmax+1];
-
-
-typedef struct statusrec{
-  bool tuneflag, chromflag, codflag, mapflag, passflag, overflag, chambre;
-  int lossplane; /* lost in: horizontal    1
-		             vertical      2
-			     longitudinal  3 */
-} statusrec;
-
+typedef char   str80[80];
 
 extern bool  stable;
 extern bool  ErrFlag;
 extern bool  trace, traceID;
-
-extern double  Fdrift1, Fkick1, Fdrift2, Fkick2, crad, cfluc;
 
 extern str80  finame,   /* input  data file  */
               foname,   /* output data file */
@@ -100,76 +39,6 @@ extern FILE  *fi,       /* lattice input  file  */
              *psin[],   /* program input file */
              *psout,              /* program output file*/
              *prr[];   /* prr[1] : input, prr[2] : output */
-
-extern statusrec status;
-
-extern bool reverse_elem;
-
-// Truncated Power Series Algebra (TPSA).
-extern const int nv_tps, nd_tps, iref_tps;
-extern int       no_tps, ndpt_tps;
-extern double    eps_tps;
-
-extern ss_vect<tps> map;
-extern MNF_struct   MNF;
-
-extern double chi_m;
-
-
-// For tune fitting.
-#define nueps    1e-6 // Precision.
-#define nudkL    0.01 // Step.
-#define nuimax   10   // Maximum number of iterations.
-
-// For chromaticity fitting.
-#define ksieps   1e-5
-#define ksidkpL  0.01
-#define ksiimax  10
-
-//* For dispersion fitting.
-#define dispeps  1e-10
-#define dispdkL  0.001
-#define dispimax 10
-#define npeakmax 10
-
-// Dynamical aperture.
-#define px_0     0.0
-#define py_0     0.0
-
-// 80% sigma coupling.
-#define sigma_eps sqrt((25.0/16.0-1.0)/(25.0/16.0+1.0))
-
-#define writetrack      true   /*protocol from tracking*/
-
-// getfloq.
-#define nfloq    4
-
-// inibump.
-#define dnux     0.02
-#define dnuy     0.01
-
-// TraceABN.
-#define ntrace   4
-
-typedef long ipeakbuf[npeakmax];
-typedef double peakbuf[npeakmax];
-
-struct LOC_getdynap {
-  double phi, delta;
-  long   nturn;
-  bool   floqs, lost;
-};
-
-
-extern double  FindRes_eps;
-
-struct LOC_findres {
-  int n;
-  double nux, nuy, f;
-  int *nx, *ny;
-  double eps;
-  bool found;
-} ;
 
 extern int P_eof(FILE *f);
 
