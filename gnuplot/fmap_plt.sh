1b95a7c708b894141c3c5d3f6c138d6e6f29ebd5
Bengtsson <ria34843@ws347.diamond.ac.uk>
2018-04-18 14:40:25 +0100

    Script tweak.

--- gnuplot/fmap_plt.sh
+++ gnuplot/fmap_plt.sh
@@ -9,8 +9,15 @@ prm5=${5-1}
 gnuplot << EOP
 
 home_dir = "$prm1"; N = $prm2; ps = $prm3; case  = $prm4; scale = $prm5;
-#MAX-V: 1, SLS-2: 2, DIAMOND-II: 4-BA 3, 6-BA 4, 8-BA 5, DIAMOND: 6, ALS-U 7,
-# DELTA 8.
+# MAX-V               1,
+# SLS-2               2,
+# DIAMOND-II:    4-BA 3,
+#                6-BA 4,
+#                8-BA 5,
+#             6-RB-BA 6,
+# DIAMOND             7,
+# ALS-U               8,
+# DELTA               9.
 
 file1 = (home_dir)."fmap.out";
 file2 = (home_dir)."fmapdp.out";
@@ -47,12 +54,14 @@ else if ((N == 1) && (case == 2)) \
 else if ((N == 1) && (case == 3)) \
   N_x = 51; N_y = 17; \
 else if ((N == 1) && (case == 4)) \
-  N_x = 58; N_y = 21; \
+  N_x = 57; N_y = 22; \
 else if ((N == 1) && (case == 5)) \
   N_x = 57; N_y = 23; sgn_x = -1; sgn_y = -1; \
 else if ((N == 1) && (case == 6)) \
-  N_x = 28; N_y = 13; \
+  N_x = 69; N_y = 29; sgn_y = -1; \
 else if ((N == 1) && (case == 7)) \
+  N_x = 28; N_y = 13; \
+else if ((N == 1) && (case == 8)) \
   N_x = 9; N_y = 4; sgn_x = -1; sgn_y = -1; \
 else if ((N == 20) && (case == 1)) \
   N_x = 5; N_y = 1; \
@@ -65,10 +74,12 @@ else if ((N == 6) && (case == 4)) \
 else if ((N == 6) && (case == 5)) \
   N_x = 9; N_y = 4; sgn_y = -1; \
 else if ((N == 6) && (case == 6)) \
-  N_x = 9; N_y = 4; sgn_y = -1; \
-else if ((N == 12) && (case == 7)) \
+  N_x = 12; N_y = 5; sgn_x = -1; sgn_y = -1; \
+else if ((N == 6) && (case == 7)) \
+  N_x = 12; N_y = 5; sgn_x = -1; sgn_y = -1; \
+else if ((N == 12) && (case == 8)) \
   N_x = 3; N_y = 1; \
-else if ((N == 24) && (case == 8)) \
+else if ((N == 24) && (case == 9)) \
   N_x = 2; N_y = 0; sgn_y = -1;
 #  N_x = 5; N_y = 3; 
 
@@ -90,7 +101,7 @@ else if (case == 3) \
   x_min = -5.0; x_max = 5.0; y_min = -3.0; y_max = 3.0; \
   delta_min = -3.1; delta_max = 3.1; \
 else if (case == 4) \
-  nu_x_min = 57.0; nu_x_max = 58.0; nu_y_min = 22.2; nu_y_max = 22.8; \
+  nu_x_min = 57.0; nu_x_max = 57.5; nu_y_min = 22.2; nu_y_max = 22.6; \
   x_min = -8.0; x_max = 8.0; y_min = -3.0; y_max = 3.0; \
   delta_min = -5.1; delta_max = 5.1; \
 else if (case == 5) \
@@ -98,14 +109,18 @@ else if (case == 5) \
   x_min = -3.0; x_max = 3.0; y_min = -2.0; y_max = 2.0; \
   delta_min = -3.0; delta_max = 3.0; \
 else if (case == 6) \
+  nu_x_min = 69.0; nu_x_max = 69.5; nu_y_min = 28.3; nu_y_max = 28.8; \
+  x_min = -3.0; x_max = 3.0; y_min = -2.0; y_max = 2.0; \
+  delta_min = -3.0; delta_max = 3.0; \
+else if (case == 7) \
   nu_x_min = 28.1; nu_x_max = 28.26; nu_y_min = 13.1; nu_y_max = 13.4; \
   x_min = -15.0; x_max = 15.0; y_min = -10.0; y_max = 10.0; \
   delta_min = -3.0; delta_max = 3.0; \
-else if (case == 7) \
+else if (case == 8) \
   nu_x_min = 39.0; nu_x_max = 39.5; nu_y_min = 14.0; nu_y_max = 14.5; \
   x_min = -6.0; x_max = 6.0; y_min = -4.0; y_max = 4.0; \
   delta_min = -4.0; delta_max = 4.0; \
-else if (case == 8) \
+else if (case == 9) \
   nu_x_min = 8.5; nu_x_max = 8.75; nu_y_min = 3.49; nu_y_max = 3.6; \
   x_min = -35.0; x_max = 35.0; y_min = -6.0; y_max = 6.0; \
   delta_min = -2.6; delta_max = 2.6;
