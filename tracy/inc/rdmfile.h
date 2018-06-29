26a69b63dd5823e66af8a356feb4ae0b7110d63e
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2018-06-29 15:37:13 +0100

    Added rdmfile.h.

---
+++ tracy/inc/rdmfile.h
@@ -0,0 +1,11 @@
+/* Tracy-2
+
+   J. Bengtsson, CBP, LBL      1990 - 1994   Pascal version
+                 SLS, PSI      1995 - 1997
+   M. Boege      SLS, PSI      1998          C translation
+   L. Nadolski   SOLEIL        2002          Link to NAFF, Radia field maps
+   J. Bengtsson  NSLS-II, BNL  2004 -        
+
+*/
+
+void rdmfile(const char *mfile_dat);
