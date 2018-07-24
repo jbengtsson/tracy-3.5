d6c431c3e6638bef1b17f8a14720a7d213078ac0
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2018-07-24 11:36:31 +0100

    Added plt_nl_optics.sh.

---
+++ gnuplot/plt_nl_optics.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+prm1=${1-0}
+
+gnuplot << EOP
+
+ps = $prm1; plt_I5 = 1;
+
+file_name = "eta_nl.out";
+
+f_s = 24; l_w = 2;
+if (ps == 0) \
+  set terminal x11; \
+else if (ps == 1) \
+  set terminal postscript enhanced color solid lw l_w "Times-Roman" f_s; \
+  ext = "ps"; \
+else if (ps == 2) \
+  set terminal postscript eps enhanced color solid lw l_w "Times-Roman" f_s; \
+  ext = "eps"; \
+else if (ps == 3) \
+  set terminal pdf enhanced color solid lw l_w font "Times-Roman f_s"; \
+  ext = "pdf"; \
+else if (ps == 4) \
+  set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
+  ext = "png";
+
+set grid;
+
+set style line 1 lt 1 lw 1 lc rgb "blue";
+set style line 2 lt 1 lw 1 lc rgb "green";
+set style line 3 lt 1 lw 1 lc rgb "red";
+
+if (ps) set output "plt_nl_optics_1.".(ext);
+set title "Linear Dispersion";
+set xlabel "s [m]"; set ylabel "{/Symbol h} [m]";
+set y2range [-2.0:20];
+plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
+     lc rgb "black", \
+     file_name using 3:5 title "{/Symbol h}_x" with lines ls 1, \
+     file_name using 3:7 title "{/Symbol h}_y" with lines ls 3;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "plt_nl_optics_2.".(ext);
+set title "2nd Order Dispersion";
+set xlabel "s [m]"; set ylabel "{/Symbol h} [m]";
+set y2range [-2.0:20];
+plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
+     lc rgb "black", \
+     file_name using 3:9 title "{/Symbol h}_x" with lines ls 1, \
+     file_name using 3:11 title "{/Symbol h}_y" with lines ls 3;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "plt_nl_optics_3.".(ext);
+set title "2nd Order Dispersion";
+set xlabel "s [m]"; set ylabel "{/Symbol h} [m]";
+set y2range [-2.0:20];
+plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
+     lc rgb "black", \
+     file_name using 3:13 title "{/Symbol h}_x" with lines ls 1, \
+     file_name using 3:15 title "{/Symbol h}_y" with lines ls 3;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+EOP
