0a87a4b74c2ce237a45d592574e3946afd2e8181
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2018-08-08 16:36:14 +0100

    Script tweaks.

--- gnuplot/plt_nl_optics1.sh
+++ gnuplot/plt_nl_optics1.sh
@@ -6,7 +6,7 @@ gnuplot << EOP
 
 ps = $prm1;
 
-file_name = "dbeta_deta.out";
+file_name = "dbeta_deta1.out";
 
 f_s = 24; l_w = 2;
 if (ps == 0) \
@@ -32,25 +32,34 @@ set style line 3 lt 1 lw 1 lc rgb "red";
 set style line 4 lt 1 lw 1 lc rgb "cyan";
 set style line 5 lt 1 lw 1 lc rgb "purple";
 
-if (ps) set output "plt_nl_optics_1.".(ext);
-set title "{/Symbol \266b/\266d} [m]";
+if (ps) set output "plt_nl_optics1_1.".(ext);
+set title "{/Symbol b}_x [m]";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
 plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      lc rgb "black", \
-     file_name using 3:5 title "{/Symbol \266b}_x/{/Symbol \266d}" \
-     with lines ls 1, \
-     file_name using 3:6 title "{/Symbol \266b}_y/{/Symbol \266d}" \
-     with lines ls 3;
+     file_name using 3:5 notitle with lines ls 1, \
+     file_name using 3:8 notitle with lines ls 2;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "plt_nl_optics_2.".(ext);
-set title "{/Symbol \266h}_x/{/Symbol \266d}";
+if (ps) set output "plt_nl_optics1_2.".(ext);
+set title "{/Symbol b}_y [m]";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
 plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      lc rgb "black", \
-     file_name using 3:7 notitle with lines ls 1;
+     file_name using 3:6 notitle with lines ls 3, \
+     file_name using 3:9 notitle with lines ls 5;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "plt_nl_optics1_3.".(ext);
+set title "{/Symbol h}_x [m]";
+set xlabel "s [m]"; set ylabel "";
+set y2range [-2.0:20];
+plot file_name using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
+     lc rgb "black", \
+     file_name using 3:7 notitle with lines ls 1, \
+     file_name using 3:10 notitle with lines ls 2;
 if (!ps) pause mouse "click on graph to cont.\n";
 
 EOP
