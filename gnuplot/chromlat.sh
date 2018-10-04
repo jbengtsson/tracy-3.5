dc9b24a826e80073867f181d05dc3f64896516e1
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2018-10-04 17:19:53 +0100

    Script tweak.

--- gnuplot/chromlat.sh
+++ gnuplot/chromlat.sh
@@ -30,7 +30,7 @@ set style line 1 lt 1 lw 1 lc rgb "blue";
 set style line 2 lt 1 lw 1 lc rgb "green";
 set style line 3 lt 1 lw 1 lc rgb "red";
 
-if (ps) set output "chromlat_1.ps"
+if (ps) set output "chromlat_1.".(ext)
 set title "Linear Chromaticity: {/Symbol b}_{x,y}{/Symbol \264h}_x";
 set xlabel "s [m]"; set ylabel "[m^2]";
 set y2range [-1.5:20];
@@ -42,7 +42,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_2.ps"
+if (ps) set output "chromlat_2.".(ext)
 set title "Linear Coupling: sqrt({/Symbol b}_x{/Symbol \264b}_y)";
 set xlabel "s [m]"; set ylabel "[m]";
 set y2range [-1.5:20];
@@ -51,7 +51,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      "chromlat.out" using 3:6 notitle  with lines ls 1;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_3.ps"
+if (ps) set output "chromlat_3.".(ext)
 set title "Second Order Chromaticity: d{/Symbol b}_{x,y}/d{/Symbol d\264h}_x";
 set xlabel "s [m]"; set ylabel "[m^2]";
 set y2range [-1.5:20];
@@ -63,7 +63,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      title "d{/Symbol b}_y/d{/Symbol d\264h}_x" with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_4.ps"
+if (ps) set output "chromlat_4.".(ext)
 set title "Second Order Chromaticity: {/Symbol b}_{x,y}{/Symbol \264}d{/Symbol h}_x/d{/Symbol d}";
 set xlabel "s [m]"; set ylabel "[m^2]";
 set y2range [-1.5:20];
@@ -77,7 +77,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_5.ps"
+if (ps) set output "chromlat_5.".(ext)
 set title "Second Order Chromaticity: {/Symbol b}_{x,y}{/Symbol \264}d{/Symbol h}_x/d{/Symbol d} + {/Symbol \264}d{/Symbol h}_x/d{/Symbol d}";
 set xlabel "s [m]"; set ylabel "[m^2]";
 set y2range [-1.5:20];
@@ -87,7 +87,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      "chromlat.out" using 3:(\$10+\$11) title "Ver" with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_6.ps"
+if (ps) set output "chromlat_6.".(ext)
 set title "Chromaticity";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-1.5:20];
@@ -97,7 +97,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      "chromlat.out" using 3:13 title "{/Symbol x}_y" with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_7.ps"
+if (ps) set output "chromlat_7.".(ext)
 set title "d{/Symbol b}_{x,y}/d{/Symbol d}";
 set xlabel "s [m]"; set ylabel "[m]";
 set y2range [-1.5:20];
@@ -111,7 +111,7 @@ plot "chromlat.out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "chromlat_8.ps"
+if (ps) set output "chromlat_8.".(ext)
 set title "Second Order Dispersion: d{/Symbol h}_x/d{/Symbol d}";
 set xlabel "s [m]"; set ylabel "[m]";
 set y2range [-1.5:20];
