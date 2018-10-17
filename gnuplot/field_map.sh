5142be0f5144ce10fd6ac641a2d6c7788503b5fc
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2018-10-17 14:37:34 +0100

    Added field_map.sh.

---
+++ gnuplot/field_map.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+prm1=${1-""}
+prm2=${2-0}
+
+gnuplot << EOP
+
+file_name = "$prm1";
+ps = $prm2;
+
+f_s = 24; l_w = 2;
+if (ps == 0) \
+  set terminal x11; \
+else if (ps == 1) \
+  set terminal postscript enhanced color solid lw l_w "Times-Roman" f_s; \
+  ext = "ps"; \
+else if (ps == 2) \
+  set terminal postscript eps enhanced color solid lw l_w "Times-Roman" f_s; \
+  ext = "eps"; \
+else if (ps == 3) \
+  set terminal pdf enhanced color solid lw l_w font "Times-Roman f_s"; \
+  ext = "pdf"; \
+else if (ps == 4) \
+  set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
+  ext = "png";
+
+set grid;
+
+set style line 1 lt 1 lw 1 lc rgb "blue";
+set style line 2 lt 1 lw 1 lc rgb "green";
+set style line 3 lt 1 lw 1 lc rgb "red";
+
+set view 48, 51, 1, 1;
+set palette rgbformulae 22, 13, -31;
+set clabel "%3.1f"; set key left;
+unset colorbox;
+set zlabel "";
+#set cntrparam level 25;
+#set contour base; set noztics; set key off; unset colorbox;
+
+if (ps) set output "field_map.".(ext)
+set title "Field Map: B_y [T]";
+set xlabel "x [mm]"; set ylabel "z [mm]";
+splot file_name using (1e3*\$1):(1e3*\$2):4:2 notitle \
+      w lines lt palette z;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+EOP
