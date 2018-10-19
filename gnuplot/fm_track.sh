087e60e8a05b6ec7515b67c53e3d5f554fa66709
jbengtsson <jbngtssn@gmail.com>
2018-10-19 08:54:29 +0100

    Script tweak.

--- gnuplot/fm_track.sh
+++ gnuplot/fm_track.sh
@@ -30,16 +30,40 @@ set style line 1 lt 1 lw 1 lc rgb "blue";
 set style line 2 lt 1 lw 1 lc rgb "green";
 set style line 3 lt 1 lw 1 lc rgb "red";
 
-if (ps) set output "fm_track.".(ext)
+if (ps) set output "fm_track_1.".(ext)
 set title "ID Trajectory";
 set xlabel "s [m]"; set ylabel "x [mm]";
 plot file_name using 2:(1e3*\$3) notitle w lines ls 1;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output "fm_track.".(ext)
+if (ps) set output "fm_track_2.".(ext)
 set title "ID Trajectory";
 set xlabel "s [m]"; set ylabel "p_x [mrad]";
 plot file_name using 2:(1e3*\$4) notitle w lines ls 1;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "fm_track_3.".(ext)
+set title "ID Trajectory";
+set xlabel "s [m]"; set ylabel "y [mm]";
+plot file_name using 2:(1e3*\$5) notitle w lines ls 1;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "fm_track_4.".(ext)
+set title "ID Trajectory";
+set xlabel "s [m]"; set ylabel "p_y [mrad]";
+plot file_name using 2:(1e3*\$6) notitle w lines ls 1;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "fm_track_5.".(ext)
+set title "ID Trajectory";
+set xlabel "s [m]"; set ylabel "delta";
+plot file_name using 2:7 notitle w lines ls 1;
+if (!ps) pause mouse "click on graph to cont.\n";
+
+if (ps) set output "fm_track_4.".(ext)
+set title "ID Trajectory";
+set xlabel "s [m]"; set ylabel "ct [mm]";
+plot file_name using 2:(1e3*\$8) notitle w lines ls 1;
 if (!ps) pause mouse "click on graph to cont.\n";
 
 EOP
