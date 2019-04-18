7369d9ebb7444d864d49754f2774f10f6b51a6a4
jbengtsson <jbngtssn@gmail.com>
2019-04-18 09:21:56 +0100

    Code tweak.

--- projects/src/ptc/get_dnu.cc
+++ projects/src/ptc/get_dnu.cc
@@ -18,7 +18,7 @@ int no_tps   = NO,
 const bool set_dnu  = false;
 const double
   beta_inj[] = {8.7, 2.1},
-  A_max[]    = {5e-3, 1e-3},
+  A_max[]    = {5e-3, 1.5e-3},
   twoJ[]     = {sqr(A_max[X_])/beta_inj[X_], sqr(A_max[Y_])/beta_inj[Y_]},
   delta_max  = 3e-2,
   dnu[]      = {0.1/6.0, 0.0};
