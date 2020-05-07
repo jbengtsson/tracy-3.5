148bd84f664078718ada9c283b35604d168a45d4
J.B <johan@JBs-MacBook-Pro.local>
2020-05-07 08:43:07 +0100

    Code clean-up.

--- projects/src/dnu.cmd
+++ projects/src/dnu.cmd
@@ -3,4 +3,4 @@
 # combine PBS standard output and error files
 #$ -j y
 
-$HOME/git_repos/tracy-3.5_temp/projects/src/dnu $flat_file
+$TRACY_LIB/projects/src/dnu $flat_file
--- projects/src/optics.cc
+++ projects/src/optics.cc
@@ -1559,7 +1559,7 @@ int main(int argc, char *argv[])
 
   globval.mat_meth = false;
 
-  if (!true)
+  if (true)
     Read_Lattice(argv[1]);
   else
     rdmfile(argv[1]);
