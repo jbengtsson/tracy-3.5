b0a1e62fc8250737ea6c1d8853834eea63f4a7f9
J.B <johan@JBs-MacBook-Pro.local>
2020-05-26 15:24:01 +0100

    Script tweak.

--- gnuplot/dnu_3d_plt.sh
+++ gnuplot/dnu_3d_plt.sh
@@ -50,25 +50,38 @@ set contour base;
 set view 48, 51, 1, 1;
 #set view 0, 0, 1, 1;
 
+set cntrparam level 75;
+
 if (ps) set output (home_dir)."dnu_3d_1.".(ext);
 
-set cntrparam level 75;
 set title "{/Symbol n}_x(x, {/Symbol d})";
 set xlabel "A_x [mm]"; set ylabel "{/Symbol d} [%]";
 set zlabel "";
 
-splot file using 1:(1e2*\$2):(N*(\$3+N_x)) notitle w lines lt palette z;
+splot file using 1:(1e2*\$2):(N*(\$5+N_x)) notitle w lines lt palette z;
 
 if (!ps) pause mouse "click on graph to cont.\n";
 
 if (ps) set output (home_dir)."dnu_3d_2.".(ext);
 
-set cntrparam level 75;
 set title "{/Symbol n}_y(x, {/Symbol d})";
 set xlabel "A_x [mm]"; set ylabel "{/Symbol d} [%]";
 set zlabel "";
 
-splot file using 1:(1e2*\$2):(N*(\$4+N_y)) notitle w lines lt palette z;
+splot file using 1:(1e2*\$2):(N*(\$6+N_y)) notitle w lines lt palette z;
+
+if (!ps) pause mouse "click on graph to cont.\n";
+
+
+set clabel "%3.1e"; set key right;
+
+if (ps) set output (home_dir)."dnu_3d_3.".(ext);
+
+set title "J_x(x, {/Symbol d}) [mm{/Symbol \327}rad]";
+set xlabel "A_x [mm]"; set ylabel "{/Symbol d} [%]";
+set zlabel "";
+
+splot file using 1:(1e2*\$2):(1e6*\$3) notitle w lines lt palette z;
 
 if (!ps) pause mouse "click on graph to cont.\n";
 
