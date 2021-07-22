fdd15c27dbef3015256784bf0dc2e884a409a894
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2021-07-22 14:47:52 +0200

    python tests: first lattice tests. currenlty crashing when clearing off files
    Lattice is read and first test is run. It appears that a double free could be
    happening at the end crashing the program
    
    Tracy creates an output file  during parsing. Ignoring this one

--- python/tests/lattice_test.py
+++ python/tests/lattice_test.py
@@ -1,5 +1,9 @@
 import unittest
 import libtracy as scsi
+import os
+
+t_dir = os.path.dirname(__file__)
+lattice_dir = os.path.join(t_dir, 'lattices')
 
 
 class _LatticeBasicFunctionality(unittest.TestCase):
@@ -9,16 +13,30 @@ class _LatticeBasicFunctionality(unittest.TestCase):
     #: lattice filename: overload in base class
     lattice_filename = None
 
+    def setUp(self):
+
+        lat = scsi.LatticeType()
+        lat.conf.trace        = False
+        lat.conf.reverse_elem = not False
+        lat.conf.mat_meth     = not False
+
+        lat.conf.H_exact    = False; lat.conf.quad_fringe    = False
+        lat.conf.Cavity_on  = False; lat.conf.radiation      = False
+        lat.conf.emittance  = False; lat.conf.IBS            = False
+        lat.conf.pathlength = False; lat.conf.bpm            = 0
+        lat.conf.Cart_Bend  = False; lat.conf.dip_edge_fudge = True
+
+        self.lat = lat
+
     def test0_readFile(self):
         '''Test if reading file from path works
         '''
-        lat = scsi.LatticeType()
-        lat.Lat_Read(self.lattice_filename)
+        self.lat.Lat_Read(self.lattice_filename)
 
     def test0_readInit(self):
         '''Test if reading file and init works
         '''
-        lat = scsi.LatticeType()
+        lat = self.lat
         lat.Lat_Read(self.lattice_filename)
         lat.Lat_Init()
 
@@ -28,11 +46,18 @@ class LatticeBasicFunctionality(_LatticeBasicFunctionality):
 
 
 class LatticeBasicFunctionalityB2(_LatticeBasicFunctionality):
-    lattice_filename = ''
+    '''
+
+    Todo:
+        Find a better file (e.g. no split elements, no 0 drift length)
+    '''
+    lattice_filename = os.path.join(lattice_dir,
+                                    'b2_stduser_beamports_blm_tracy_corr')
 
 
 del _LatticeBasicFunctionality
-
+# no simple example file yet
+del LatticeBasicFunctionality
 
 if __name__ == '__main__':
     unittest.main()
