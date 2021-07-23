4a5ace53bfb072dddc02d4128fc79e919fad1b01
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2021-07-23 10:34:31 +0200

    python/setup.py gsl-config used for obtaining gsl prerequisites
    This rather slim approach follows https://github.com/mattpitkin/lintegrate.
    It will work on posix platforms.Not using pygsl gsl_Extension as this is
    rather hefty.

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import subprocess
 from setuptools import setup
 from distutils.file_util import copy_file
 
@@ -14,17 +16,47 @@ copy_file(
     update=True
 )
 
+
 # execute gsl-config --libs to see what is required
-gsl_lib_dirs = ['/usr/lib/x86_64-linux-gnu']
-gsl_libs = ['gsl', 'gslcblas', 'm']
-gsl_include = []
+# inspired by https://github.com/mattpitkin/lintegrate
+def gsl_config(*args, **kwargs):
+    """Run gsl-config and return pre-formatted output
+    """
+    gsl_config = 'gsl-config'
+
+    if os.name == 'nt':
+        cmd = "{} {}".format([gsl_config, " ".join(args)])
+        kwargs.setdefault("shell", True)
+
+    else:
+        cmd = [gsl_config] + list(args)
+
+    output = subprocess.check_output(cmd, **kwargs)
+    return output.decode("utf-8").strip()
+
+
+# execute gsl-config --cflags to get include dir
+gsl_include = gsl_config("--cflags")[2:]
+
+
+# execute gsl-config --libs for dir and libs
+# assuming posix compatabilitly
+_gsl_lib = gsl_config("--libs").split(" ")
+# assumption :
+gsl_lib_dir = _gsl_lib[0][2:]
+gsl_libs = [ll[2:] for ll in _gsl_lib if ll[:2] == '-l']
+del _gsl_lib
+
+logger = logging.getLogger('setup')
+logger.info('Using GSL: include %s library_dir %s libraries %s',
+            gsl_include, gsl_lib_dir, gsl_libs)
 
 ext_modules = [
     Pybind11Extension(
         "libtracy",
         sorted(['src/tracy_py.cc']),
-        include_dirs=['../tracy/inc'] + gsl_include,
-        library_dirs=['../tracy/src/.libs'] + gsl_lib_dirs,
+        include_dirs=['../tracy/inc'] + [gsl_include],
+        library_dirs=['../tracy/src/.libs'] + [gsl_lib_dir],
         libraries=['tracy'] + gsl_libs
     ),
 ]
