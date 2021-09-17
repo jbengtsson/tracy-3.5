911f46c270906c949bf62a6a9165e65cd43725bc
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2021-09-17 15:56:37 +0200

    Python interface: example partly working
    Fixed get_eps_x. Arrays created internally.
    Started to make tst.py a bit more pythonic.
    Consider that objects provide a proper
    dunder str and repr

--- python/src/tracy_py.cc
+++ python/src/tracy_py.cc
@@ -48,7 +48,7 @@ public:
   get_eps_x_py(const bool prt) {
     double eps_x, sigma_delta, U_0;
     // TODO: allocate appropriate space
-    std::vector<double> J(3, 0.0), tau(3, 0.0), I(3, 0.0);
+    std::vector<double> J(3, 0.0), tau(3, 0.0), I(6, 0.0);
 
     get_eps_x(eps_x, sigma_delta, U_0, J, tau, I, prt);
 
