94936da21a7f5492ae60055aee62e341e6865c4f
J.B <jbngtssn@gmail.com>
2023-03-14 07:42:33 +0100

    Tweaked plt_wakefield_2.sh.

--- projects/src/plt_wakefield_2.sh
+++ projects/src/plt_wakefield_2.sh
@@ -4,11 +4,9 @@ prm1=${1-0}
 
 gnuplot << EOP
 
-c0 = 2.99792458e8
-
 ps = $prm1
 
-file_name = "wakefield.out"
+file_name = "track.out"
 
 f_s = 24
  l_w = 2
@@ -34,7 +32,7 @@ set style line 1 lt 1 lw 1 lc rgb "blue"  pointsize point_size
 set style line 2 lt 1 lw 1 lc rgb "green" pointsize point_size
 set style line 3 lt 1 lw 1 lc rgb "red"   pointsize point_size
 
-if (ps) set output "plt_wakefield_1.".(ext)
+if (ps) set output "plt_wakefield_2_1.".(ext)
 
 set multiplot
 
@@ -44,19 +42,19 @@ set key right bottom
 set title "<x> [{/Symbol m}m]"
 set xlabel "turn no"
 set ylabel ""
-plot file_name using 1:(1e6*\$2) notitle with points ls 1
+plot file_name using 1:(1e6*\$2) notitle with linespoints ls 1
 
 set origin 0.0, 0.0
 set title "<y> [{/Symbol m}m]"
 set xlabel "turn no"
 set ylabel ""
-plot file_name using 1:(1e6*\$4) notitle with points ls 3
+plot file_name using 1:(1e6*\$4) notitle with linespoints ls 3
 
 unset multiplot
 if (!ps) pause mouse "click on graph to cont.\n"
 
 
-if (ps) set output "plt_wakefield_2.".(ext)
+if (ps) set output "plt_wakefield_2_2.".(ext)
 
 set multiplot
 
@@ -64,18 +62,20 @@ set origin 0.0, 0.5
 set title "<s> [ps]"
 set xlabel "turn no"
 set ylabel ""
-plot file_name using 1:(1e12*\$7/c0) notitle with points ls 1
+plot file_name using 1:(1e12*\$7) notitle with linespoints ls 1
 
 set origin 0.0, 0.0
 set title "<{/Symbol d}>"
 set xlabel "turn no"
 set ylabel ""
-plot file_name using 1:6 notitle with points ls 3
+plot file_name using 1:6 notitle with linespoints ls 3
 
 unset multiplot
 if (!ps) pause mouse "click on graph to cont.\n"
 
-if (ps) set output "plt_wakefield_3.".(ext)
+exit
+
+if (ps) set output "plt_wakefield_2_3.".(ext)
 
 set multiplot
 
@@ -105,7 +105,7 @@ set origin 0.0, 0.5
 set title "{/Symbol s}_s [ps]"
 set xlabel "turn no"
 set ylabel ""
-plot file_name using 1:(1e12*\$13/c0) notitle with points ls 1
+plot file_name using 1:(1e12*\$13) notitle with points ls 1
 
 set origin 0.0, 0.0
 set title "{/Symbol s_d}"
