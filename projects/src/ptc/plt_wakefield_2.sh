2e90d9bd2b109f52ac9331836e0f5605a0032aef
J.B <jbngtssn@gmail.com>
2023-04-05 09:55:46 +0200

    Script clean-up: wakefield_2.sh.

--- projects/src/ptc/plt_wakefield_2.sh
+++ projects/src/ptc/plt_wakefield_2.sh
@@ -6,7 +6,7 @@ gnuplot << EOP
 
 ps = $prm1
 
-file_name = "moments.out"
+file_name = "track.out"
 
 f_s = 24
  l_w = 2
@@ -39,13 +39,13 @@ set multiplot
 set size 1.0, 0.5
 set origin 0.0, 0.5
 set key right bottom
-set title "<x> [{/Symbol m}m]"
+set title "x [{/Symbol m}m]"
 set xlabel "Turn no"
 set ylabel ""
 plot file_name using 1:(1e6*\$2) notitle with points ls 1
 
 set origin 0.0, 0.0
-set title "<y> [{/Symbol m}m]"
+set title "y [{/Symbol m}m]"
 set xlabel "Turn no"
 set ylabel ""
 plot file_name using 1:(1e6*\$4) notitle with points ls 3
@@ -58,13 +58,13 @@ if (ps) set output "plt_wakefield_2.".(ext)
 set multiplot
 
 set origin 0.0, 0.5
-set title "<t> [ps]"
+set title "t [ps]"
 set xlabel "Turn no"
 set ylabel ""
 plot file_name using 1:(1e12*\$7) notitle with points ls 1
 
 set origin 0.0, 0.0
-set title "<{/Symbol d}>"
+set title "{/Symbol d}"
 set xlabel "Turn no"
 set ylabel ""
 plot file_name using 1:6 notitle with points ls 3
@@ -75,63 +75,6 @@ if (!ps) pause mouse "click on graph to cont.\n"
 if (ps) set output "plt_wakefield_3.".(ext)
 
 set multiplot
-
-set size 1.0, 0.5
-set origin 0.0, 0.5
-set key right bottom
-set title "{/Symbol s}_x [{/Symbol m}m]"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:(1e6*\$9) notitle with points ls 1
-
-set origin 0.0, 0.0
-set title "{/Symbol s}_y [{/Symbol m}m]"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:(1e6*\$11) notitle with points ls 3
-
-unset multiplot
-if (!ps) pause mouse "click on graph to cont.\n"
-
-
-if (ps) set output "plt_wakefield_4.".(ext)
-
-set multiplot
-
-set origin 0.0, 0.5
-set title "{/Symbol s}_t [ps]"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:(1e12*\$14) notitle with points ls 1
-
-set origin 0.0, 0.0
-set title "{/Symbol s_d}"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:13 notitle with points ls 3
-
-unset multiplot
-if (!ps) pause mouse "click on graph to cont.\n"
-
-
-if (ps) set output "plt_wakefield_5.".(ext)
-
-set multiplot
-
-set origin 0.0, 0.5
-set title "HOM Amplitude"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:15 notitle with points ls 1
-
-set origin 0.0, 0.0
-set title "HOM Phase"
-set xlabel "Turn no"
-set ylabel ""
-plot file_name using 1:16 notitle with points ls 3
-
-unset multiplot
-if (!ps) pause mouse "click on graph to cont.\n"
 
 EOP
 
