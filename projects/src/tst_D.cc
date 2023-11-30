68a5461dad4322a17aa3a8dd1388dcec98f1ba7e
Johan <jbngtssn@gmail.com>
2023-11-30 09:52:12 -0500

    Code clean-up.

--- projects/src/tst_D.cc
+++ projects/src/tst_D.cc
@@ -10,6 +10,9 @@ int no_tps = NO;
 
 void tst_D(void)
 {
+  // Average over one half synchrotron oscillation.
+  const int n_turn = 215;
+
   long int        lastpos;
   ss_vect<double> ps, cod;
   ss_vect<tps>    A, M, D, Sigma;
@@ -22,7 +25,11 @@ void tst_D(void)
   ps.zero();
   ps += cod;
   globval.Diff_mat.zero();
-  Cell_Pass(0, globval.Cell_nLoc, ps, lastpos);
+  printf("\n  n_turn = %d\n", n_turn);
+  for (int k = 0; k < n_turn; k++)
+    Cell_Pass(0, globval.Cell_nLoc, ps, lastpos);
+  for (int k = 0; k < 2*nd_tps; k++)
+    globval.Diff_mat[k] /= n_turn;
 
   cout << scientific << setprecision(3)
        << "\ncod:\n" << setw(11) << cod << "\n";
@@ -52,21 +59,23 @@ void tst_D(void)
   printf("\nDiffusion Matrix - Floquet Space:\n");
   prt_lin_map(3, D);
 
-  D =  A*D*tp_map(3, A);
+  D = A*D*tp_map(3, A);
 
-  printf("\nDiffusion Matrix - Phase Space:\n");
+  printf("\nDiffusion Matrix - Phase Space:");
   prt_lin_map(3, D);
 
-  printf("\nDiffusion Matrix - Beam Envelope Theory:\n");
+  printf("\nDiffusion Matrix - Beam Envelope Theory:");
+  printf("\nD - Phase Space:");
   prt_lin_map(3, globval.Diff_mat);
 
+  printf("\nD - Floquet Space:");
+  prt_lin_map(3, Inv(A)*globval.Diff_mat*Inv(tp_map(3, A)));
 
   Sigma.zero();
   for (int k = 0; k < 2*nd_tps; k++)
     Sigma[k] = globval.eps[k/2]*tps(0e0, k+1);
 
-  printf("\nSigma - Floquet Space:\n");
-
+  printf("\nSigma - Floquet Space:");
   prt_lin_map(3, Sigma);
 
   Sigma = A*Sigma*tp_map(3, A);
@@ -109,6 +118,9 @@ int main(int argc, char *argv[])
 
   Ring_GetTwiss(true, 0e0);
   printglob();
+  prt_lat("linlat1.out", globval.bpm, true);
+  prt_lat("linlat.out", globval.bpm, true, 10);
+
 
   tst_D();
 }
