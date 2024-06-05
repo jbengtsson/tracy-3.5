10bd672e70ab9e432668b8097120f57df14517ca
Johan <jbngtssn@gmail.com>
2024-06-05 09:31:17 +0200

    Code clean-up.

--- projects/src/ptc/max_4u.cc
+++ projects/src/ptc/max_4u.cc
@@ -24,11 +24,28 @@ void set_ps_rot(const double dnu_x, const double dnu_y)
 }
 
 
-std::vector<double> get_k_ijklm(void)
+tps get_h_local(const ss_vect<tps> &map, const bool dragt_finn)
+{
+  ss_vect<tps>  map1, R;
+
+  if (dragt_finn)
+    // Dragt-Finn factorization.
+    return LieFact_DF(map, R);
+  else {
+    // Single Lie exponent.
+    danot_(1);
+    map1 = map;
+    danot_(no_tps);
+    return LieFact(map*Inv(map1));
+  }
+}
+
+
+std::vector<double> get_h_ijklm(void)
 {
   long int            lastpos;
-  std::vector<double> k_ijklm;
-  tps                 K_re, K_im;
+  std::vector<double> h_buf;
+  tps                 h, h_re, h_im;
   ss_vect<tps>        Id_scl, map;
 
   Id_scl.identity();
@@ -40,50 +57,49 @@ std::vector<double> get_k_ijklm(void)
   map.identity();
   Cell_Pass(0, globval.Cell_nLoc, map, lastpos);
   danot_(no_tps);
-  MNF = MapNorm(map, 1);
+  h = get_h_local(map, true);
+  CtoR(h*Id_scl, h_re, h_im);
 
-  CtoR(MNF.K*Id_scl, K_re, K_im);
+  h_buf.push_back(h_ijklm(h_re, 1, 1, 0, 0, 0));
+  h_buf.push_back(h_ijklm(h_re, 1, 1, 1, 1, 0));
+  h_buf.push_back(h_ijklm(h_re, 0, 0, 1, 1, 0));
 
-  k_ijklm.push_back(h_ijklm(K_re, 1, 1, 0, 0, 0));
-  k_ijklm.push_back(h_ijklm(K_re, 1, 1, 1, 1, 0));
-  k_ijklm.push_back(h_ijklm(K_re, 0, 0, 1, 1, 0));
+  h_buf.push_back(h_ijklm(h_re, 1, 1, 0, 0, 2));
+  h_buf.push_back(h_ijklm(h_re, 0, 0, 1, 1, 2));
 
-  k_ijklm.push_back(h_ijklm(K_re, 1, 1, 0, 0, 2));
-  k_ijklm.push_back(h_ijklm(K_re, 0, 0, 1, 1, 2));
-
-  return k_ijklm;
+  return h_buf;
 }
 
 
-void prt_k_ijklm
+void prt_h_ijklm
 (ostream &outf, const std::vector<double> dnu,
- const std::vector<double> &k_ijklm)
+ const std::vector<double> &h_ijklm)
 {
   outf << fixed << setprecision(3) << setw(7) << dnu[X_]  << setw(7) << dnu[Y_];
-  for (int k = 0; k < k_ijklm.size(); k++)
-    outf << scientific << setprecision(3) << setw(11) << k_ijklm[k];
+  for (int k = 0; k < h_ijklm.size(); k++)
+    outf << scientific << setprecision(3) << setw(11) << h_ijklm[k];
   outf << "\n";
 }
 
 
-double get_chi_2(std::vector<double> &k_ijklm)
+double get_chi_2(std::vector<double> &h_ijklm)
 {
   double chi_2 = 0e0;
 
-  for (int k = 0; k < k_ijklm.size(); k++)
-    chi_2 += sqr(k_ijklm[k]);
+  for (int k = 0; k < h_ijklm.size(); k++)
+    chi_2 += sqr(h_ijklm[k]);
   return chi_2;
 }
 
-void scan_k_ijklm(const int n, const double dnu_max_x, const double dnu_max_y)
+void scan_h_ijklm(const int n, const double dnu_max_x, const double dnu_max_y)
 {
   const double
     dnu_max[] = {dnu_max_x, dnu_max_y};
   const string
-    file_name = "k_ijklm.dat"; 
+    file_name = "h_ijklm.dat"; 
 
   double              nu_step[2], chi_2, chi_2_min = 1e30;
-  std::vector<double> k_ijklm, dnu, dnu_min, k_ijklm_min;
+  std::vector<double> h_ijklm, dnu, dnu_min, h_ijklm_min;
   ofstream            outf;
 
   file_wr(outf, file_name.c_str());
@@ -94,7 +110,7 @@ void scan_k_ijklm(const int n, const double dnu_max_x, const double dnu_max_y)
   }
 
   for (int k = 0; k < 5; k++)
-    k_ijklm_min.push_back(1e30);
+    h_ijklm_min.push_back(1e30);
 
   printf("\n");
   for (int j = 0; j < 2*n+1; j++) {
@@ -103,16 +119,16 @@ void scan_k_ijklm(const int n, const double dnu_max_x, const double dnu_max_y)
       if (k == 0)
 	outf << "\n";
       set_ps_rot(dnu[X_], dnu[Y_]);
-      k_ijklm = get_k_ijklm();
-      chi_2 = get_chi_2(k_ijklm);
+      h_ijklm = get_h_ijklm();
+      chi_2 = get_chi_2(h_ijklm);
       if (chi_2 < chi_2_min) {
 	dnu_min = dnu;
 	chi_2_min = chi_2;
       }
       for (int l = 0; l < 5; l++)
-	k_ijklm_min[l] = min(fabs(k_ijklm[l]), k_ijklm_min[l]);
+	h_ijklm_min[l] = min(fabs(h_ijklm[l]), h_ijklm_min[l]);
 
-      prt_k_ijklm(outf, dnu, k_ijklm);
+      prt_h_ijklm(outf, dnu, h_ijklm);
 
       dnu[Y_] += nu_step[Y_];
     }
@@ -122,14 +138,14 @@ void scan_k_ijklm(const int n, const double dnu_max_x, const double dnu_max_y)
 
   outf.close();
 
-  cout << scientific << setprecision(3) << "\ndk_ijklm rms = "
+  cout << scientific << setprecision(3) << "\ndh_ijklm rms = "
        << setw(9) << sqrt(chi_2_min) << "\n";
   set_ps_rot(dnu_min[X_], dnu_min[Y_]);
-  k_ijklm = get_k_ijklm();
-  prt_k_ijklm(cout, dnu_min, k_ijklm);
-  cout << "\nk_ijklm_min  = ";
-  for (int k = 0; k < k_ijklm_min.size(); k++)
-    cout << scientific << setprecision(3) << setw(10) << k_ijklm_min[k];
+  h_ijklm = get_h_ijklm();
+  prt_h_ijklm(cout, dnu_min, h_ijklm);
+  cout << "\nh_ijklm_min  = ";
+  for (int k = 0; k < h_ijklm_min.size(); k++)
+    cout << scientific << setprecision(3) << setw(10) << h_ijklm_min[k];
   cout << "\n";
 }
 
@@ -168,14 +184,14 @@ int main(int argc, char *argv[])
     printglob();
   }
 
-  if (false) {
+  if (!false) {
     // Twiss functions are needed for set_ps_rot.
     Ring_GetTwiss(true, 0e0);
     printglob();
-    scan_k_ijklm(10, 1.0, 1.0);
+    scan_h_ijklm(10, 1.0, 1.0);
   }
 
-  if (!false) {
+  if (false) {
     // Twiss functions are needed for set_ps_rot.
     Ring_GetTwiss(true, 0e0);
     printglob();
