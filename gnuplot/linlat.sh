0048fc243bfa7bffc44ff07eaba58668917cc137
Johan <jbngtssn@gmail.com>
2024-06-06 12:49:28 +0200

    Added benchmark for chromatic correction scheme.

--- gnuplot/linlat.sh
+++ gnuplot/linlat.sh
@@ -52,11 +52,23 @@ plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      file_name.".out" using 3:13 title "{/Symbol h}_y" with lines ls 3;
 if (!ps) pause mouse "click on graph to cont.\n";
 
+if (ps) set output file_name."_3.".(ext);
+set title "Linear Chromaticity: {/Symbol b}_{x,y} x {/Symbol h}_x";
+set xlabel "s [m]"; set ylabel "[m^2]";
+set y2range [-1.5:20];
+plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
+     lc rgb "black", \
+     file_name.".out" using 3:(\$6*\$8) title "{/Symbol b}_x x {/Symbol h}_x" \
+     with lines ls 1, \
+     file_name.".out" using 3:(\$11*\$8) title "{/Symbol b}_y x {/Symbol h}_x" \
+     with lines ls 3;
+if (!ps) pause mouse "click on graph to cont.\n";
+
 exit;
 
 # Symbol characters broken for > \219.
 
-if (ps) set output file_name."_3.".(ext);
+if (ps) set output file_name."_4.".(ext);
 set title "{/Symbol b}_{x,y}{/Symbol \264h}_x";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -75,7 +87,7 @@ set palette model RGB file file_name_palette \
 unset colorbox;
 #set cbrange [-2.0:1.5];
 
-if (ps) set output file_name."_4.".(ext);
+if (ps) set output file_name."_5.".(ext);
 set title "{/ZapfChancery-MediumItalic H}_x({/Symbol h}_x\\\~, \
     {/Symbol h}\'_x\\\~)";
 set xlabel "{/Symbol h}_x\\\~ [10^{-3}]";
@@ -86,7 +98,7 @@ plot file_name.".out" using (1e3*\$15):(1e3*\$16):(abs(\$4)) notitle \
      "{/Symbol n}_x" with lines lt palette z;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output file_name."_5.".(ext);
+if (ps) set output file_name."_6.".(ext);
 set title "sqrt({/ZapfChancery-MediumItalic H}_x(s)*{/Symbol b}_x)";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -98,7 +110,7 @@ if (!ps) pause mouse "click on graph to cont.\n";
 
 set size nosquare;
 
-if (ps) set output file_name."_6.".(ext);
+if (ps) set output file_name."_7.".(ext);
 set title "Normalized Phase Advance";
 set xlabel "s [m]"; set ylabel "{/Symbol n}";
 set y2range [-2.0:20];
@@ -110,7 +122,7 @@ if (!ps) pause mouse "click on graph to cont.\n";
 
 exit;
 
-if (ps) set output file_name."_7.".(ext);
+if (ps) set output file_name."_8.".(ext);
 set title "{/Symbol h}_x [m]";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -119,7 +131,7 @@ plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      file_name.".out" using 3:8 notitle with lines ls 1;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output file_name."_8.".(ext);
+if (ps) set output file_name."_9.".(ext);
 set title "{/Symbol h}'_x";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -128,7 +140,7 @@ plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      file_name.".out" using 3:9 notitle with lines ls 1;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output file_name."_9.".(ext);
+if (ps) set output file_name."_10.".(ext);
 set title "eta{_x\\\~";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -138,7 +150,7 @@ plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      file_name.".out" using 3:16 title "eta'_x\\\~" with lines ls 2;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output file_name."_10.".(ext);
+if (ps) set output file_name."_11.".(ext);
 set title "{/ZapfChancery-MediumItalic H}_x(s)";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -148,7 +160,7 @@ plot file_name.".out" using 3:4 axis x1y2 notitle with fsteps lt 1 lw 1 \
      lines lt palette z;
 if (!ps) pause mouse "click on graph to cont.\n";
 
-if (ps) set output file_name."_11.".(ext);
+if (ps) set output file_name."_12.".(ext);
 set title "arg\\\{{/ZapfChancery-MediumItalic H}_x(s)\\\}";
 set xlabel "s [m]"; set ylabel "";
 set y2range [-2.0:20];
@@ -162,7 +174,7 @@ if (!ps) pause mouse "click on graph to cont.\n";
 
 exit;
 
-if (ps) set output file_name."_12.".(ext);
+if (ps) set output file_name."_13.".(ext);
 set title "{/Symbol g}";
 set xlabel "s [m]"; set ylabel "{/Symbol g}";
 set y2range [-2.0:20];
@@ -176,7 +188,7 @@ if (!ps) pause mouse "click on graph to cont.\n";
 
 exit;
 
-if (ps) set output file_name."_13.".(ext);
+if (ps) set output file_name."_14.".(ext);
  set title "{/Symbol a}"; \
 set xlabel "s [m]"; set ylabel "{/Symbol a}"; \
 set y2range [-2.0:20]; \
