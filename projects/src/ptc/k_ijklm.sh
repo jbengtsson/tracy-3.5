d1ab8fbeb45326820d0d891b53d9c75ec364e72d
Johan <jbngtssn@gmail.com>
2024-06-10 01:50:06 +0200

    Changed to the Matlab jet font for contour plot.

--- projects/src/ptc/k_ijklm.sh
+++ projects/src/ptc/k_ijklm.sh
@@ -5,14 +5,15 @@ prm2=${2-"k_ijklm"}
 
 gnuplot << EOP
 
-ps        = $prm1;
-file_name = "$prm2";
-contour   = 0;
+ps        = $prm1
+file_name = "$prm2"
+contour   = 1
 
-f_s = 24; l_w = 2;
+f_s = 24
+l_w = 2
 # Enhanced is needed for Greek characters.
 if (ps == 0) \
-  set terminal qt 0 enhanced font "Sans, 9"; \
+  set terminal qt 0 enhanced font "Sans, 9" \
 else if (ps == 1) \
   set terminal postscript enhanced color solid lw l_w "Times-Roman" f_s; \
   ext = "ps"; \
@@ -26,68 +27,78 @@ else if (ps == 4) \
   set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
   ext = "png";
 
-set grid;
+set grid
 
-set style line 1 lt 1 lw 1 lc rgb "blue";
-set style line 2 lt 1 lw 1 lc rgb "green";
-set style line 3 lt 1 lw 1 lc rgb "red";
+set style line 1 lt 1 lw 1 lc rgb "blue"
+set style line 2 lt 1 lw 1 lc rgb "green"
+set style line 3 lt 1 lw 1 lc rgb "red"
 
-if (contour) \
-  set nosurface; \
-  set noztics;
+# file_name_jet = "`echo $TRACY_LIB`/gnuplot/jet"
+# Load 64-color palette for Jet
+# set palette model RGB file file_name_jet using (\$1/255):(\$2/255):(\$3/255)
 
-#set cntrlabel format "%4.1f";
-set key left;
-#unset cntrlabel;
-#set noztics;
-unset colorbox;
-#set cbrange [0:1];
+set cntrparam level 25
+set view map
+set pm3d
 
-#set cntrparam level 18;
-set cntrparam level 25;
+set macros
+JET="define (0 0 0 0.5, 1./8 0 0 1, 3./8 0 1 1, 5./8 1 1 0, 7./8 1 0 0, 1 0.5 0 0)"
 
-# x <-> horizontal, y <-> vertical, z <-> perpendicular to screen.
-# rot_x, rot_z, scale, scale_z.
-if (!contour) set view 65, 15, 1, 1;
-if (contour) set view 0, 0, 1, 1;
+MATLAB = "defined (0  0.0 0.0 0.5, \
+                   1  0.0 0.0 1.0, \
+                   2  0.0 0.5 1.0, \
+                   3  0.0 1.0 1.0, \
+                   4  0.5 1.0 0.5, \
+                   5  1.0 1.0 0.0, \
+                   6  1.0 0.5 0.0, \
+                   7  1.0 0.0 0.0, \
+                   8  0.5 0.0 0.0 )"
 
-set palette rgbformulae 22, 13, -31 negative;
 
-if (ps) set output file_name."_1.".(ext);
-if (ps) set output "tune_scan.ps"
+set palette @JET
 
-set title "k_{22000}";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$5)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+if (ps) set output file_name."_1.".(ext)
+set title "k_{22000}"
+set xlabel "{/Symbol n}_x"
+ set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$5)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
 
-set title "k_{11110}";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$6)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+set cbrange [-20:-10]
 
-set title "k_{00220}";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$7)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+if (ps) set output file_name."_2.".(ext)
+set title "k_{11110}"
+set xlabel "{/Symbol n}_x"
+ set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$6)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
 
-set title "k_{11002}";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$8)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+if (ps) set output file_name."_3.".(ext)
+set title "k_{00220}"
+set xlabel "{/Symbol n}_x"
+set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$7)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
 
-set title "k_{00112}";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$9)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+if (ps) set output file_name."_4.".(ext)
+set title "k_{ijklm} RMS"
+set xlabel "{/Symbol n}_x"
+ set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$8)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
 
-set title "k_{ijklm} RMS";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-splot file_name.".dat" using 1:2:(log(\$10)) notitle with lines lt \
-      palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+unset cbrange [-30:-14]
+
+if (ps) set output file_name."_5.".(ext)
+set title "k_{11002}"
+set xlabel "{/Symbol n}_x"
+ set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$9)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
+
+if (ps) set output file_name."_6.".(ext)
+set title "k_{00112}"
+set xlabel "{/Symbol n}_x"
+ set ylabel "{/Symbol n}_y"
+splot file_name.".dat" using 1:2:(log(\$10)) notitle with pm3d
+if (!ps) pause mouse "click on graph to cont.\n"
