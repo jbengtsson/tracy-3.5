d7d068ae202aab0f9ca10251319fd1c571662488
Johan <jbngtssn@gmail.com>
2024-10-12 22:36:08 +0200

    Added check of sympletic map.

--- projects/src/for_stephen.cc
+++ projects/src/for_stephen.cc
@@ -9,6 +9,48 @@
 int no_tps = NO;
 
 
+void chk_sympl(void)
+{
+  int long     lastpos;
+  ss_vect<tps> M, S, diff;
+
+  M.identity();
+  Cell_Pass(0, globval.Cell_nLoc, M, lastpos);
+
+  S = get_S(3);
+  diff = tp_map(3, M)*S*M;
+  printf("\ndiff:");
+  prt_lin_map(3, diff);
+}
+
+
+void compute_mat(void)
+{
+  const bool   incremental = false;
+  const string file_name   = "for_stephen.txt";
+
+  long int     lastpos;
+  ss_vect<tps> M;
+
+  // Redirect stdout to text file.
+  freopen(file_name.c_str(), "w", stdout); 
+
+  M.identity();
+  for (auto k = 0; k <= globval.Cell_nLoc; k++) {
+    if (!incremental)
+      M.identity();
+    Cell_Pass(k, k, M, lastpos);
+    printf("\n  %2d %10s S = %7.3f", k, Cell[k].Elem.PName, Cell[k].S);
+    if (Cell[k].Elem.Pkind == Mpole)
+      printf("  h = %21.16e", Cell[k].Elem.M->Pirho);
+    prt_lin_map(3, M);
+  }
+
+  // Restore stdout.
+  freopen("/dev/tty", "w", stdout);
+}
+
+
 void set_state(void)
 {
   globval.H_exact        = false;
@@ -24,33 +66,10 @@ void set_state(void)
 }
 
 
-void compute_mat(void)
-{
-  const bool   incremental = false;
-  const string file_name   = "for_stephen.txt";
-
-  long int     lastpos;
-  ss_vect<tps> M;
-
-  // Redirect stdout to text file.
-  freopen(file_name.c_str(), "a+", stdout); 
-
-  M.identity();
-  for (auto k = 0; k <= globval.Cell_nLoc; k++) {
-    if (not incremental)
-      M.identity();
-    Cell_Pass(k, k, M, lastpos);
-    printf("\n  %2d %10s %7.3f", k, Cell[k].Elem.PName, Cell[k].S);
-    prt_lin_map(3, M);
-  }
-
-  // Restore stdout.
-  freopen("/dev/tty", "w", stdout);
-}
-
-
 int main(int argc, char *argv[])
 {
+  double I[6], eps_x, sigma_delta, U_0, J[3], tau[3];
+
   globval.mat_meth = !false;
 
   if (true)
@@ -66,5 +85,16 @@ int main(int argc, char *argv[])
   prt_lat("linlat1.out", globval.bpm, true);
   prt_lat("linlat.out", globval.bpm, true, 10);
 
-  compute_mat();
+  if (!false) {
+    if (!globval.mat_meth)
+      GetEmittance(ElemIndex("cav"), false, true);
+    else
+      get_eps_x(eps_x, sigma_delta, U_0, J, tau, I, true);
+  }
+
+  if (!false)
+    compute_mat();
+
+  if (false)
+    chk_sympl();
 }
