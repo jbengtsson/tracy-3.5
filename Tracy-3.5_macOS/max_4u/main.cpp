212b71a528c8a5bcfbce4069eb691a1a33228ae1
Johan <jbngtssn@gmail.com>
2025-01-11 02:37:37 +0100

    Built tracy_lib, tracy_ptc_lib, max_4u, and drv_terms.

---
+++ Tracy-3.5_macOS/max_4u/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  max_4u
+//
+//  Created by J.B. on 1/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
