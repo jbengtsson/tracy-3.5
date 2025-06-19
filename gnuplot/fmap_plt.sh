abb0777cb4fdb2dacbad902e3797b90e95b7bb20
Johan <jbngtssn@gmail.com>
2025-06-19 16:58:36 +0200

    Script refactoring.

--- gnuplot/fmap_plt.sh
+++ gnuplot/fmap_plt.sh
@@ -8,21 +8,22 @@ prm5=${5-1}
 
 gnuplot << EOP
 
-home_dir = "$prm1"; N = $prm2; ps = $prm3; case = $prm4; scale = $prm5;
+home_dir = "$prm1"
+N        = $prm2
+ps       = $prm3
+case     = $prm4
+scale    = $prm5
 
-nan = -2.0;
+nan = -2.0
 
-# BESSY-III  1..3,
-# ALS-U      4,
-# SLS-2      5.
-
-file1 = (home_dir)."fmap.out";
-file2 = (home_dir)."fmapdp.out";
+file1 = (home_dir)."fmap.out"
+file2 = (home_dir)."fmapdp.out"
 
 # Only works for postscript terminal.
 #set fontpath "/usr/share/fonts/msttcore"
 
-f_s = 36; l_w = 2;
+f_s = 36
+l_w = 2
 if (ps == 0) \
   set terminal qt 0 font "Sans, 9"; \
 else if (ps == 1) \
@@ -36,67 +37,52 @@ else if (ps == 3) \
   ext = "pdf"; \
 else if (ps == 4) \
   set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
-  ext = "png";
+  ext = "png"
 
 # sgn:
 #    frac(nu):
 #       [0,   0.5]   1
 #       [0.5, 1.0]  -1, int(nu) = nu + 1.
 
-sgn_x = 1; sgn_y = 1;
+sgn_x = -1
+sgn_y = -1
 if ((N == 1) && (case == 1)) \
-  N_x = 43; N_y = 13; sgn_y = -1;\
-else if ((N == 16) && (case == 1)) \
-  N_x = 3; N_y = 1; sgn_x = -1; sgn_y = -1; \
-else if ((N == 20) && (case == 2)) \
-  N_x = 3; N_y = 1; sgn_x = -1; sgn_y = -1; \
-else if ((N == 20) && (case == 3)) \
-  N_x = 3; N_y = 1; sgn_x = -1; sgn_y = -1; \
-else if ((N == 12) && (case == 4)) \
-  N_x = 3; N_y = 1; \
-else if ((N == 12) && (case == 5)) \
-  N_x = 3; N_y = 1;
+  N_x = 58; \
+  N_y = 18; \
+else if ((N == 20) && (case == 1)) \
+  N_x = 3; \
+  N_y = 1
 
-if (case == 1) \
-  nu_x_min = 42.55; nu_x_max = 43.95; nu_y_min = 12.45; nu_y_max = 13.05; \
-  x_max = 5.0; y_max = 5.0; delta_max = 4.0; \
-else if (case == 2) \
-  nu_x_min = 53.0; nu_x_max = 54.0; nu_y_min = 11.0; nu_y_max = 12.0; \
-  x_max = 3.0; y_max = 3.0; delta_max = 3.5; \
-else if (case == 3) \
-  nu_x_min = 52.0; nu_x_max = 53.0; nu_y_min = 16.0; nu_y_max = 17.0; \
-  x_max = 3.0; y_max = 3.0; delta_max = 3.5; \
-else if (case == 4) \
-  nu_x_min = 40.0; nu_x_max = 41.0; nu_y_min = 14.0; nu_y_max = 15.0; \
-  x_max = 4.0; y_max = 4.0; delta_max = 4.0; \
-else if (case == 5) \
-  nu_x_min = 37.5; nu_x_max = 38.5; nu_y_min = 14.0; nu_y_max = 15.0; \
-  x_max = 8.0; y_max = 8.0; delta_max = 4.0;
+nu_x_min = 57.0
+nu_x_max = 58.5
+nu_y_min = 17.0
+nu_y_max = 17.5
 
-# DELTA: del008 .
-#  nu_x_min = 9.0; nu_x_max = 9.3; nu_y_min = 3.1; nu_y_max = 3.4; 
+x_max = 5.0
+y_max = 5.0
+delta_max = 4.0
 
+set grid
 
-set grid;
+set style line 1 lw 1 lc rgb "red"
+set style line 2 lw 1 lc rgb "dark-orange"
+set style line 3 lw 1 lc rgb "blue"
+set style line 4 lw 1 lc rgb "dark-green"
+set style line 5 lw 1 lc rgb "purple"
+set style line 6 lw 1 lc rgb "cyan"
 
-set style line 1 lw 1 lc rgb "red";
-set style line 2 lw 1 lc rgb "dark-orange";
-set style line 3 lw 1 lc rgb "blue";
-set style line 4 lw 1 lc rgb "dark-green";
-set style line 5 lw 1 lc rgb "purple";
-set style line 6 lw 1 lc rgb "cyan";
-
-file_name = "`echo $TRACY_LIB`/gnuplot/jet.dat";
+file_name = "`echo $TRACY_LIB`/gnuplot/jet.dat"
 # Load 64-color palette for Jet
-set palette model RGB file file_name using (\$1/255):(\$2/255):(\$3/255);
+set palette model RGB file file_name using (\$1/255):(\$2/255):(\$3/255)
 
-set cbrange [-10:1];
-set noztics; unset clabel;
-set view map;
+set cbrange [-10:1]
+set noztics
+unset clabel
+set view map
 # To set y-axis to left side and avoid compression of color box.
-unset pm3d;
+unset pm3d
 
-set parametric;
+set parametric
 
 # Compute n for the different resonances:
 #
@@ -107,94 +93,102 @@ set parametric;
 # The adjustment is the difference between x_min/x_max and y_min/y_max
 # vs. the actual (nu_x,y) values
 
-i10    = floor(nu_x_min) + 1.0;
-i01_1  = floor(nu_y_min) + 1.0;
-i01_2  = floor(nu_y_min) + 2.0;
+i10    = floor(nu_x_min) + 1.0
+i01_1  = floor(nu_y_min) + 1.0
+i01_2  = floor(nu_y_min) + 2.0
 
-i20    = floor(nu_x_min) + 1.5;
-i02_1  = floor(nu_y_min) + 0.5;
-i02_2  = floor(nu_y_min) + 1.5;
-i02_3  = floor(nu_y_min) + 2.5;
+i20    = floor(nu_x_min) + 1.5
+i02_1  = floor(nu_y_min) + 0.5
+i02_2  = floor(nu_y_min) + 1.5
+i02_3  = floor(nu_y_min) + 2.5
 
-i11    = floor(nu_x_min+nu_y_max);
-i1m1_1 = floor(nu_x_min-nu_y_min);
-i1m1_2 = floor(nu_x_min-nu_y_min) - 1.0;
+i11    = floor(nu_x_min+nu_y_max)
+i1m1_1 = floor(nu_x_min-nu_y_min)
+i1m1_2 = floor(nu_x_min-nu_y_min) - 1.0
 
-i30    = floor(nu_x_min) + 4.0/3.0;
-i12_1  = floor(nu_x_min+2*nu_y_max) - 2.0;
-i12_2  = floor(nu_x_min+2*nu_y_max) - 1.0;
-i12_3  = floor(nu_x_min+2*nu_y_max);
-i12_4  = floor(nu_x_min+2*nu_y_max) + 1.0;
-i1m2_1 = floor(nu_x_min-2*nu_y_min) - 2.0;
-i1m2_2 = floor(nu_x_min-2*nu_y_min) - 1.0;
-i1m2_3 = floor(nu_x_min-2*nu_y_min) - 2.0;
-i1m2_4 = floor(nu_x_min-2*nu_y_min) + 1.0;
+i30    = floor(nu_x_min) + 4.0/3.0
+i12_1  = floor(nu_x_min+2*nu_y_max) - 2.0
+i12_2  = floor(nu_x_min+2*nu_y_max) - 1.0
+i12_3  = floor(nu_x_min+2*nu_y_max)
+i12_4  = floor(nu_x_min+2*nu_y_max) + 1.0
+i1m2_1 = floor(nu_x_min-2*nu_y_min) - 2.0
+i1m2_2 = floor(nu_x_min-2*nu_y_min) - 1.0
+i1m2_3 = floor(nu_x_min-2*nu_y_min) - 2.0
+i1m2_4 = floor(nu_x_min-2*nu_y_min) + 1.0
 
-i40    = floor(nu_x_min) + 1.25;
-i04_1  = floor(nu_y_min) + 0.25;
-i04_2  = floor(nu_y_min) + 1.25;
-i04_3  = floor(nu_y_min) + 1.75;
-i04_4  = floor(nu_y_min) + 2.25;
-i22_1  = floor(2.0*nu_x_min+2.0*nu_y_max) - 2.0;
-i22_2  = floor(2.0*nu_x_min+2.0*nu_y_max);
-i2m2_1 = floor(2.0*nu_x_min-2.0*nu_y_min) - 1.0;
-i2m2_2 = floor(2.0*nu_x_min-2.0*nu_y_min);
-i2m2_3 = floor(2.0*nu_x_min-2.0*nu_y_min) + 1.0;
+i40    = floor(nu_x_min) + 1.25
+i04_1  = floor(nu_y_min) + 0.25
+i04_2  = floor(nu_y_min) + 1.25
+i04_3  = floor(nu_y_min) + 1.75
+i04_4  = floor(nu_y_min) + 2.25
+i22_1  = floor(2.0*nu_x_min+2.0*nu_y_max) - 2.0
+i22_2  = floor(2.0*nu_x_min+2.0*nu_y_max) - 1.0
+i22_3  = floor(2.0*nu_x_min+2.0*nu_y_max)
+i22_4  = floor(2.0*nu_x_min+2.0*nu_y_max) + 1.0
+i22_5  = floor(2.0*nu_x_min+2.0*nu_y_max) + 2.0
+i2m2_1 = floor(2.0*nu_x_min-2.0*nu_y_min) - 1.0
+i2m2_2 = floor(2.0*nu_x_min-2.0*nu_y_min)
+i2m2_3 = floor(2.0*nu_x_min-2.0*nu_y_min) + 1.0
+i2m2_4 = floor(2.0*nu_x_min-2.0*nu_y_min) + 2.0
 
-i14_1  = floor(nu_x_min+4.0*nu_y_max) - 5.0;
-i14_2  = floor(nu_x_min+4.0*nu_y_max) - 4.0;
-i14_3  = floor(nu_x_min+4.0*nu_y_max) - 3.0;
-i14_4  = floor(nu_x_min+4.0*nu_y_max) - 2.0;
-i14_5  = floor(nu_x_min+4.0*nu_y_max) - 1.0;
-i14_6  = floor(nu_x_min+4.0*nu_y_max);
-i14_7  = floor(nu_x_min+4.0*nu_y_max) + 1.0;
-i32_1  = floor(3.0*nu_x_min+2.0*nu_y_max);
-i32_2  = floor(3.0*nu_x_min+2.0*nu_y_max) + 1.0;
-i32_3  = floor(3.0*nu_x_min+2.0*nu_y_max) + 2.0;
-i1m4_1 = floor(nu_x_min-4.0*nu_y_min);
-i1m4_2 = floor(nu_x_min-4.0*nu_y_min) - 1.0;
-i1m4_3 = floor(nu_x_min-4.0*nu_y_min) - 2.0;
-i1m4_4 = floor(nu_x_min-4.0*nu_y_min) - 3.0;
-i1m4_5 = floor(nu_x_min-4.0*nu_y_min) - 4.0;
-i1m4_6 = floor(nu_x_min-4.0*nu_y_min) - 5.0;
-i3m2_1 = floor(3.0*nu_x_min-2.0*nu_y_min);
-i3m2_2 = floor(3.0*nu_x_min-2.0*nu_y_min) + 1.0;
-i3m2_3 = floor(3.0*nu_x_min-2.0*nu_y_min) + 2.0;
+i14_1  = floor(nu_x_min+4.0*nu_y_max) - 5.0
+i14_2  = floor(nu_x_min+4.0*nu_y_max) - 4.0
+i14_3  = floor(nu_x_min+4.0*nu_y_max) - 3.0
+i14_4  = floor(nu_x_min+4.0*nu_y_max) - 2.0
+i14_5  = floor(nu_x_min+4.0*nu_y_max) - 1.0
+i14_6  = floor(nu_x_min+4.0*nu_y_max)
+i14_7  = floor(nu_x_min+4.0*nu_y_max) + 1.0
+i32_1  = floor(3.0*nu_x_min+2.0*nu_y_max)
+i32_2  = floor(3.0*nu_x_min+2.0*nu_y_max) + 1.0
+i32_3  = floor(3.0*nu_x_min+2.0*nu_y_max) + 2.0
+i1m4_1 = floor(nu_x_min-4.0*nu_y_min)
+i1m4_2 = floor(nu_x_min-4.0*nu_y_min) - 1.0
+i1m4_3 = floor(nu_x_min-4.0*nu_y_min) - 2.0
+i1m4_4 = floor(nu_x_min-4.0*nu_y_min) - 3.0
+i1m4_5 = floor(nu_x_min-4.0*nu_y_min) - 4.0
+i1m4_6 = floor(nu_x_min-4.0*nu_y_min) - 5.0
+i3m2_1 = floor(3.0*nu_x_min-2.0*nu_y_min)
+i3m2_2 = floor(3.0*nu_x_min-2.0*nu_y_min) + 1.0
+i3m2_3 = floor(3.0*nu_x_min-2.0*nu_y_min) + 2.0
 
-i21    = floor(2.0*nu_x_min+nu_y_min) + 2.0;
-i2m1_1 = floor(2.0*nu_x_min-nu_y_min);
-i2m1_2 = floor(2.0*nu_x_min-nu_y_min) + 1.0;
+i21    = floor(2.0*nu_x_min+nu_y_min) + 2.0
+i2m1_1 = floor(2.0*nu_x_min-nu_y_min)
+i2m1_2 = floor(2.0*nu_x_min-nu_y_min) + 1.0
 
-#i60    = floor(nu_x_min);
-i06    = floor(nu_y_min) + 14.0/6.0;
-#i51    = floor(5.0*nu_x_min+1.0*nu_y_max);
-#i5m1   = floor(5.0*nu_x_min-1.0*nu_y_min);
-i15    = floor(1.0*nu_x_min+5.0*nu_y_max);
-i1m5_1 = floor(1.0*nu_x_min-5.0*nu_y_min) - 6;
-i1m5_2 = floor(1.0*nu_x_min-5.0*nu_y_min) - 7;
-#i24    = floor(2.0*nu_x_min+4.0*nu_y_max);
-i2m4   = floor(2.0*nu_x_min-4.0*nu_y_min) - 5;
-i42    = floor(4.0*nu_x_min+2.0*nu_y_max) + 2;
-i4m2   = floor(4.0*nu_x_min-2.0*nu_y_min) - 1;
-i33    = floor(3.0*nu_x_min+3.0*nu_y_max) + 1;
-#i3m3   = floor(3.0*nu_x_min-3.0*nu_y_min) - 3;
+#i60    = floor(nu_x_min)
+i06    = floor(nu_y_min) + 14.0/6.0
+#i51    = floor(5.0*nu_x_min+1.0*nu_y_max)
+#i5m1   = floor(5.0*nu_x_min-1.0*nu_y_min)
+i15    = floor(1.0*nu_x_min+5.0*nu_y_max)
+i1m5_1 = floor(1.0*nu_x_min-5.0*nu_y_min) - 6
+i1m5_2 = floor(1.0*nu_x_min-5.0*nu_y_min) - 7
+#i24    = floor(2.0*nu_x_min+4.0*nu_y_max)
+i2m4   = floor(2.0*nu_x_min-4.0*nu_y_min) - 5
+i42    = floor(4.0*nu_x_min+2.0*nu_y_max) + 2
+i4m2   = floor(4.0*nu_x_min-2.0*nu_y_min) - 1
+i33    = floor(3.0*nu_x_min+3.0*nu_y_max) + 1
+#i3m3   = floor(3.0*nu_x_min-3.0*nu_y_min) - 3
 
-#i6m4   = floor(6.0*nu_x_min-4.0*nu_y_min) - 2;
+#i6m4   = floor(6.0*nu_x_min-4.0*nu_y_min) - 2
 
-set urange [nu_x_min:nu_x_max]; set vrange [nu_y_min:nu_y_max];
+set urange [nu_x_min:nu_x_max]
+set vrange [nu_y_min:nu_y_max]
 
-if (ps) set output "fmap_1.".(ext);
+if (ps) set output "fmap_1.".(ext)
 
-#set multiplot;
+set multiplot
 
-unset contour;
-unset pm3d;
-set colorbox;
+unset contour
+unset pm3d
+set colorbox
 
-#set size 1.0, 0.5; set origin 0.0, 0.5;
-set title "Tune Shift";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-if (scale) set xrange [nu_x_min:nu_x_max]; set yrange [nu_y_min:nu_y_max]; 
+set size 1.0, 0.5
+set origin 0.0, 0.5
+set title "Tune Shift"
+set xlabel "{/Symbol n}_x"
+set ylabel "{/Symbol n}_y"
+if (scale) set xrange [nu_x_min:nu_x_max]
+set yrange [nu_y_min:nu_y_max] 
 splot "fmap.out" using \
       ((abs(\$3-int(\$3)) > 1e-6)? N*(N_x+sgn_x*\$3) : NaN): \
       ((abs(\$4-int(\$4)) > 1e-6)? N*(N_y+sgn_y*\$4) : NaN):7 \
@@ -224,9 +218,13 @@ splot "fmap.out" using \
       i40, v,                    1.0 notitle with lines ls 3, \
       u,     (i22_1-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
       u,     (i22_2-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_3-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_4-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_5-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_1)/2.0, 1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_2)/2.0, 1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_3)/2.0, 1.0 notitle with lines ls 3, \
+      u,     (2.0*u-i2m2_4)/2.0, 1.0 notitle with lines ls 3, \
                                                               \
       u,     (i32_1-3.0*u)/2.0,  1.0 notitle with lines ls 4, \
       u,     (i32_2-3.0*u)/2.0,  1.0 notitle with lines ls 4, \
@@ -250,10 +248,10 @@ splot "fmap.out" using \
                                                               \
       u,     i21-2.0*u,          1.0 notitle with lines ls 6, \
       u,     2.0*u-i2m1_1,       1.0 notitle with lines ls 6, \
-      u,     2.0*u-i2m1_2,       1.0 notitle with lines ls 6;
+      u,     2.0*u-i2m1_2,       1.0 notitle with lines ls 6
 
 #                                                              \
-#      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5;
+#      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5
 #      u,     i06,                1.0 notitle with lines ls 5, \
 #      u,     (i15-u)/5.0,        1.0 notitle with lines ls 5, \
 #      u,     (u-i1m5_1)/5.0,     1.0 notitle with lines ls 5, \
@@ -261,37 +259,41 @@ splot "fmap.out" using \
 #      u,     (2.0*u-i2m4)/4.0,   1.0 notitle with lines ls 5, \
 #      u,     (i42-4.0*u)/2.0,    1.0 notitle with lines ls 5, \
 #      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5, \
-#      u,     (i33-3.0*u)/3.0,    1.0 notitle with lines ls 5;
+#      u,     (i33-3.0*u)/3.0,    1.0 notitle with lines ls 5
 
-#      u,     (6.0*u-i6m4)/4.0,   1.0 notitle with lines ls 5;
+#      u,     (6.0*u-i6m4)/4.0,   1.0 notitle with lines ls 5
 
-if (!ps) pause mouse "click on graph to cont.\n";
+set pm3d at b map
+#set contour
+#unset colorbox
 
-if (ps) set output "fmap_2.".(ext);
+set origin 0.0, 0.0
+set title "Diffusion Map"
+set xlabel "x [mm]"
+set ylabel "y [mm]"
+if (scale) set xrange [-x_max:x_max]
+set yrange [-y_max:y_max]
+splot file1 using 1:2:((\$7 != nan)? \$7 : NaN) notitle lt palette z
 
-set pm3d at b map;
-#set contour;
-#unset colorbox;
+unset multiplot
 
-#set origin 0.0, 0.0;
-set title "Diffusion Map";
-set xlabel "x [mm]"; set ylabel "y [mm]";
-if (scale) set xrange [-x_max:x_max]; set yrange [-y_max:y_max];
-splot file1 using 1:2:((\$7 != nan)? \$7 : NaN) notitle lt palette z;
+if (!ps) pause mouse "click on graph to cont.\n"
 
-if (!ps) pause mouse "click on graph to cont.\n";
+if (ps) set output (home_dir)."fmap_2.".(ext)
 
-if (ps) set output (home_dir)."fmap_3.".(ext);
+set multiplot
 
-unset contour;
+unset contour
 # To set y-axis to left side and avoid compression of color box.
-unset pm3d;
-set colorbox;
+unset pm3d
+set colorbox
 
-#set size 1.0, 0.5; set origin 0.0, 0.5;
-set title "Tune Shift";
-set xlabel "{/Symbol n}_x"; set ylabel "{/Symbol n}_y";
-if (scale) set xrange [nu_x_min:nu_x_max]; set yrange [nu_y_min:nu_y_max];
+set origin 0.0, 0.5
+set title "Tune Shift"
+set xlabel "{/Symbol n}_x"
+set ylabel "{/Symbol n}_y"
+if (scale) set xrange [nu_x_min:nu_x_max]
+set yrange [nu_y_min:nu_y_max]
 splot "fmapdp.out" using \
       ((abs(\$3-int(\$3)) > 1e-6)? N*(N_x+sgn_x*\$3) : NaN): \
       ((abs(\$4-int(\$4)) > 1e-6)? N*(N_y+sgn_y*\$4) : NaN):7 \
@@ -321,9 +323,13 @@ splot "fmapdp.out" using \
       i40, v,                    1.0 notitle with lines ls 3, \
       u,     (i22_1-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
       u,     (i22_2-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_3-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_4-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
+      u,     (i22_5-2.0*u)/2.0,  1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_1)/2.0, 1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_2)/2.0, 1.0 notitle with lines ls 3, \
       u,     (2.0*u-i2m2_3)/2.0, 1.0 notitle with lines ls 3, \
+      u,     (2.0*u-i2m2_4)/2.0, 1.0 notitle with lines ls 3, \
                                                               \
       u,     (i32_1-3.0*u)/2.0,  1.0 notitle with lines ls 4, \
       u,     (i32_2-3.0*u)/2.0,  1.0 notitle with lines ls 4, \
@@ -347,10 +353,10 @@ splot "fmapdp.out" using \
                                                               \
       u,     i21-2.0*u,          1.0 notitle with lines ls 6, \
       u,     2.0*u-i2m1_1,       1.0 notitle with lines ls 6, \
-      u,     2.0*u-i2m1_2,       1.0 notitle with lines ls 6;
+      u,     2.0*u-i2m1_2,       1.0 notitle with lines ls 6
 
 #                                                              \
-#      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5;
+#      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5
 #      u,     i06,                1.0 notitle with lines ls 5, \
 #      u,     (i15-u)/5.0,        1.0 notitle with lines ls 5, \
 #      u,     (u-i1m5_1)/5.0,     1.0 notitle with lines ls 5, \
@@ -358,24 +364,22 @@ splot "fmapdp.out" using \
 #      u,     (2.0*u-i2m4)/4.0,   1.0 notitle with lines ls 5, \
 #      u,     (i42-4.0*u)/2.0,    1.0 notitle with lines ls 5, \
 #      u,     (4.0*u-i4m2)/2.0,   1.0 notitle with lines ls 5, \
-#      u,     (i33-3.0*u)/3.0,    1.0 notitle with lines ls 5;
+#      u,     (i33-3.0*u)/3.0,    1.0 notitle with lines ls 5
 
-#      u,     (6.0*u-i6m4)/4.0,   1.0 notitle with lines ls 5;
+#      u,     (6.0*u-i6m4)/4.0,   1.0 notitle with lines ls 5
 
-if (!ps) pause mouse "click on graph to cont.\n";
+set pm3d at b map
+#unset colorbox
 
-if (ps) set output "fmap_4.".(ext);
+set origin 0.0, 0.0
+set title "Diffusion Map"
+set xlabel "{/Symbol d} [%]"
+set ylabel "x [mm]"
+if (scale) set xrange \
+  [-delta_max:delta_max]; \
+  set yrange [-x_max:x_max]
+splot file2 using 1:2:((\$7 != nan)? \$7 : NaN) notitle lt palette z
 
-set pm3d at b map;
-#unset colorbox;
-
-#set origin 0.0, 0.0;
-set title "Diffusion Map";
-set xlabel "{/Symbol d} [%]"; set ylabel "x [mm]";
-if (scale) set xrange [-delta_max:delta_max]; set yrange [-x_max:x_max];
-splot file2 using 1:2:((\$7 != nan)? \$7 : NaN) notitle lt palette z;
-
-#unset multiplot;
-if (!ps) pause mouse "click on graph to cont.\n";
+if (!ps) pause mouse "click on graph to cont.\n"
 
 EOP
