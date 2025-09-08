f31e444f5dc7c8ca1e8de4cb46ed059fd1a5528a
Johan <jbngtssn@gmail.com>
2025-09-08 16:39:55 +0200

    Cleaned up the autoconf build files.

--- make_tracy-3.5.sh
+++ make_tracy-3.5.sh
@@ -6,16 +6,22 @@ rm -rf autom4te.cache
 rm -rf aclocal.m4
 rm -rf tracy/lib/*
 
+# Create config dir – if it doesn't exist.
+mkdir -p config
+
 make distclean
 
 # Configure libtool (for shared libraries).
-# Linux.
-# libtoolize
-# Macbook.
-glibtoolize
-
-# Create config dir – if it doesn't exist.
-mkdir -p config
+case "$(uname -s)" in
+    Linux*)
+        echo "Running on Linux"
+        libtoolize
+        ;;
+    Darwin*)
+	echo "Running on macOS"
+        glibtoolize
+	;;
+esac
 
 ./bootstrap
 ./configure --prefix=$dir/tracy
--- projects/make_projects.sh
+++ projects/make_projects.sh
@@ -5,10 +5,10 @@ dir=`pwd`
 rm -rf autom4te.cache
 rm -rf aclocal.m4
 
-make distclean
-
 # Create config dir – if it doesn't exist.
 mkdir -p config
+
+make distclean
 
 ./bootstrap
 ./configure --prefix=$dir/projects
