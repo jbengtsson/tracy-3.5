93a933703afd82edd96abf7126768e669ec57d8e
Johan <jbngtssn@gmail.com>
2025-10-02 11:41:25 +0200

    Script refactoring.

--- gnuplot/H_long.sh
+++ gnuplot/H_long.sh
@@ -6,24 +6,26 @@ gnuplot << EOP
 
 ps = $prm1;
 
+f_s = 14
+l_w = 2
 
-f_s = 24;
-l_w = 2;
 # Enhanced is needed for Greek characters.
-if (ps == 0) \
-  set terminal qt 0 enhanced font "Sans, 9"; \
-else if (ps == 1) \
-  set terminal postscript enhanced color solid lw l_w "Times-Roman" f_s; \
-  ext = "ps"; \
-else if (ps == 2) \
-  set terminal postscript eps enhanced color solid lw l_w "Times-Roman" f_s; \
-  ext = "eps"; \
-else if (ps == 3) \
-  set terminal pdf enhanced color solid lw l_w font "Times-Roman f_s"; \
-  ext = "pdf"; \
-else if (ps == 4) \
-  set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
+if (ps == 0) {
+  set terminal qt enhanced font "DejaVu Sans,12"
+} else if (ps == 1) {
+  set terminal postscript enhanced color solid lw l_w font "Times-Roman,".f_s
+  ext = "ps"
+} else if (ps == 2) {
+  set terminal postscript eps enhanced color solid lw l_w font \
+ "Times-Roman,".f_s
+  ext = "eps"
+} else if (ps == 3) {
+  set terminal pdfcairo enhanced color solid lw l_w font "Times-Roman,".f_s
+  ext = "pdf"
+} else if (ps == 4) {
+  set terminal pngcairo enhanced color solid lw l_w font "Times-Roman,".f_s
   ext = "png"
+}
 
 set grid
 
@@ -39,8 +41,8 @@ if (ps) set output "H_long.".(ext)
 
 set title "Longitudinal Phase Space to O({/Symbol a}_4)"
 # Greek letters doesn't work for terminal.
-set xlabel "phase [deg]" \
-# Degree symbol does not work for qt terminal \
+set xlabel "phase [deg]"
+# Degree symbol does not work for qt terminal.
 # set xlabel "{/Symbol f} [{/Symbol \260}]"
 set ylabel "{/Symbol d} [%]"
 splot "H_long.dat" using 1:2:3 notitle with lines lt palette z
