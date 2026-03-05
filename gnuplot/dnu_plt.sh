e3ca998db8fb77dd61534a74852a005a4dd7cd78
Johan <jbngtssn@gmail.com>
2026-03-05 15:05:28 +0100

    Script tweak.

--- gnuplot/dnu_plt.sh
+++ gnuplot/dnu_plt.sh
@@ -41,7 +41,9 @@ else if (ps == 4) \
 if ((N == 1) && (case == 1)) \
   N_x = 51; N_y = 22; \
 else if ((N == 1) && (case == 2)) \
-  N_x = 28; N_y = 3; \
+  N_x = 26; N_y = 5; \
+else if ((N == 1) && (case == 3)) \
+  N_x = 19; N_y = 9; \
 else if ((N == 20) && (case == 1)) \
   N_x = 2; N_y = 0; \
 else if ((N == 20) && (case == 2)) \
