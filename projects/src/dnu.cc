c9de55647add0ea160ba4190a37e872ca9c80a97
Johan <jbngtssn@gmail.com>
2026-06-10 15:56:20 +0200

    Use case tweak.

--- projects/src/dnu.cc
+++ projects/src/dnu.cc
@@ -10,8 +10,8 @@ const int
 
 const double
 #if 1
-  A_max[]   = {4e-3, 4e-3},
-  delta_max = 6e-2;
+  A_max[]   = {6e-3, 6e-3},
+  delta_max = 3e-2;
 #else
   A_max[]   = {6e-3, 2.5e-3},
   delta_max = 3.5e-2;
@@ -30,7 +30,7 @@ void set_state(void)
   globval.Aperture_on    = false;
   globval.Cart_Bend      = false;
   globval.dip_edge_fudge = true;
-  globval.EPU            = false;
+  globval.EPU            = !false;
 }
 
 
