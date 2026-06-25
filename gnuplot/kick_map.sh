6bc1ee6c8f7d0760e242c469608265c7725d5287
Johan <jbngtssn@gmail.com>
2026-06-25 13:13:07 +0200

    Added scale option.

--- gnuplot/kick_map.sh
+++ gnuplot/kick_map.sh
@@ -2,11 +2,13 @@
 
 prm1=${1-""}
 prm2=${2-0}
+prm3=${3-1}
 
 gnuplot << EOP
 
-file_name = "$prm1";
-ps        = $prm2;
+file_name = "$prm1"
+ps        = $prm2
+scl       = $prm3
 
 f_s = 24;
 l_w = 2;
@@ -26,37 +28,37 @@ else if (ps == 4) \
   set term pngcairo enhanced color solid lw l_w font "Times-Roman f_s"; \
   ext = "png";
 
-set grid;
+set grid
 
-set style line 1 lt 1 lw 1 lc rgb "blue";
-set style line 2 lt 1 lw 1 lc rgb "green";
-set style line 3 lt 1 lw 1 lc rgb "red";
+set style line 1 lt 1 lw 1 lc rgb "blue"
+set style line 2 lt 1 lw 1 lc rgb "green"
+set style line 3 lt 1 lw 1 lc rgb "red"
 
-set view 50, 348, 1, 1;
-set palette rgbformulae 22, 13, -31;
-set cntrlabel format "%3.1f";
-set key left;
-set zlabel "";
-#set cntrparam level 25;
-#set contour base;
-#set noztics;
-#set key off;
-#unset colorbox;
+set view 50, 348, 1, 1
+set palette rgbformulae 22, 13, -31
+set cntrlabel format "%3.1f"
+set key left
+set zlabel ""
+#set cntrparam level 25
+#set contour base
+#set noztics
+#set key off
+#unset colorbox
 
 if (ps) set output "kick_map_1.".(ext)
-set title "Kick Map: {/Symbol q}_x [mrad]";
-set xlabel "x [mm]";
-set ylabel "y [mm]";
-splot file_name using (1e3*\$1):(1e3*\$2):(1e3*\$3):(1e3*\$3) notitle \
-      w lines lt palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+set title "Kick Map: {/Symbol q}_x [mrad]"
+set xlabel "x [mm]"
+set ylabel "y [mm]"
+splot file_name using (1e3*\$1):(1e3*\$2):(scl*1e3*\$3):(scl*1e3*\$3) notitle \
+      w lines lt palette z
+if (!ps) pause mouse "click on graph to cont.\n"
 
 if (ps) set output "kick_map_2.".(ext)
-set title "Kick Map: {/Symbol q}_y [mrad]";
-set xlabel "x [mm]";
-set ylabel "y [mm]";
-splot file_name using (1e3*\$1):(1e3*\$2):(1e3*\$4):(1e3*\$4) notitle \
-      w lines lt palette z;
-if (!ps) pause mouse "click on graph to cont.\n";
+set title "Kick Map: {/Symbol q}_y [mrad]"
+set xlabel "x [mm]"
+set ylabel "y [mm]"
+splot file_name using (1e3*\$1):(1e3*\$2):(scl*1e3*\$4):(scl*1e3*\$4) notitle \
+      w lines lt palette z
+if (!ps) pause mouse "click on graph to cont.\n"
 
 EOP
